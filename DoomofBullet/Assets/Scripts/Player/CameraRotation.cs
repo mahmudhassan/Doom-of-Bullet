dbd101320291e02fee60c00be29ce733612d8329
clundstedt225 <conlundstedt@gmail.com>
2018-07-17 21:07:23 -0400

    Made UI Movement Functional + Added small test area

---
+++ DoomofBullet/Assets/Scripts/Player/CameraRotation.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour {
+
+    //This script is placed on the players camera
+
+    //Grab Rotation Screen Field
+    public FixedTouchField RotationField;
+    
+    //Vars
+    Vector2 mouseLook;
+    Vector2 smoothV;
+    public float sensitivity = 3.0f;
+    public float smoothing = 1.0f;
+    GameObject character;
+
+
+    // Use this for initialization
+    void Start () {
+        //Get character to rotate left to right only
+        character = this.transform.parent.gameObject;
+
+        // [KEEP DISABLED FOR PC TESTING & ENABLED FOR MOBILE/TOUCH]
+        // (On start hide and lock cursor position)
+        //Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //Grabs from the FixedTouchField instead of mouse x / y for mobile controll
+        var md = new Vector2(RotationField.TouchDist.x, RotationField.TouchDist.y);
+
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        mouseLook += smoothV;
+
+        //Locks ability to look past stright up and down
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, Vector3.up);
+    }
+}
---
+++ DoomofBullet/Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    //Mobile Touch Input Components
+    public FixedJoystick MoveJoystick;
+    public FixedButton JumpButton;
+    //public FixedButton CrouchButton;
+
+    //Vars & Properties
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    CharacterController controller;
+
+    // Use this for initialization
+    void Start () {
+        //Get The Controller From The Player GameObject
+        controller = GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        //Check for ground to prevent jump abuse
+        if (controller.isGrounded)
+        {
+            //Grab input from MoveJoystick reference
+            moveDirection = new Vector3(MoveJoystick.inputVector.x, 0, MoveJoystick.inputVector.y);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            //Jumping Logic (grab input from jump button reference)
+            if (JumpButton.Pressed)
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        //Apply movements and simulate gravity
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
