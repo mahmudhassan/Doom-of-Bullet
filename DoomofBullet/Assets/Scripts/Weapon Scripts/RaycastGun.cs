fc5c8857215e66050be88d0d441881f45933e82a
Zerrelyn <37177482+Zerrelyn@users.noreply.github.com>
2018-09-10 22:47:38 +0200

    Worked on weapon scripts
    Changed input key.
    Added new ammo system and added reload.
    Added Impact effect and bullet impactForce.

--- DoomofBullet/Assets/Scripts/Weapon Scripts/RaycastGun.cs
+++ DoomofBullet/Assets/Scripts/Weapon Scripts/RaycastGun.cs
@@ -13,25 +13,62 @@ public class RaycastGun : MonoBehaviour {
     public float recoilParam = 0.2f, maxRecoil_xParam = 10f, recoilSpeedParam = 10f;
     public bool isAutomatic = false;
     bool readyToFire = true;
-    public float rateOfFire = 1.0f;
+    public float rateOfFire = 5.0f;
     public float damageAmount = 2;
+    public float range = 100f;
     public int clipSize = 30;
     public int clips = 5; //ammo refills use up one clip 
     int ammo = 0;
     float lastFired;
 
-	// Use this for initialization
-	void Start () {
+    private float nextTimeToFire = 0f;
+    public float impactForce = 30f;
+    public GameObject impactEffect;
+    public Text text;
+
+    public int maxAmmo = 30;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    public bool isReloading = false;
+
+    // Use this for initialization
+    void Start () {
         //Fill to initial clip size
         ammo = clipSize;
 
         recoilComponent = gameObject.GetComponent<GunRecoil>();
 
         //fireButton = ??? need to get ref at runtime?
+
+        currentAmmo = maxAmmo;
     }
 	
 	// Update is called once per frame
 	void Update () {
+        text.text = "Ammo: " + currentAmmo + "/30";
+
+        if (isReloading)
+            return;
+
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+        else if(currentAmmo < 30 && Input.GetKeyDown("r"))
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + (1f / rateOfFire);
+            shoot();
+            currentAmmo -= 1;
+        }
+
+        /*
         if (isAutomatic)
         { //Full-Automatic Path for ballistic & hitscan
             if (fireButton.Pressed && ammo > 0)
@@ -60,6 +97,20 @@ public class RaycastGun : MonoBehaviour {
                 }
             }
         } //Closes main structure   
+        */
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+
+        Debug.Log("Reloading");
+
+        yield return new WaitForSeconds(reloadTime);
+
+        currentAmmo = maxAmmo;
+
+        isReloading = false;
     }
 
     public void shoot() {
@@ -70,13 +121,28 @@ public class RaycastGun : MonoBehaviour {
         recoilComponent.StartRecoil(recoilParam, maxRecoil_xParam, recoilSpeedParam);
 
         Debug.DrawRay(gunTip.position, gunTip.forward * 500, Color.yellow);
-        weaponSound.Play();
-        flash.Play();
+        if(weaponSound != null)
+            weaponSound.Play();
+        if(flash != null)
+            flash.Play();
 
         //What Happens When Ray Hits Player 
-        if (Physics.Raycast(ray, out hit, 500))
+        if (Physics.Raycast(ray, out hit, range))
         {
             Debug.Log("The ray hit a sossy: " + hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damageAmount);
+            }
+
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
         }
     }
 
